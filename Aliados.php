2052f1e3f4063af31600e0207ea064a9f16d4861
Jovanny Leobardo Salas <jovannyleobardo10@gmail.com>
2018-08-10 09:47:27 -0500

    Agregando primer avance

---
+++ Aliados.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Aliados</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/InnovaWeekend.css">
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      	  <div class="form-group">
+          <div class="row col-md-12">
+            <a class="navbar-brand" href="index.html" class="img-responsive"><img src="imagenes/isaac.png" class="img-responsive"></a>
+          </div>
+        </div>
+          <a class="navbar-brand" href="index.html">INICIO</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+               <ul class="navbar-nav">
+                  <li class="nav-item active">
+                     <a class="nav-link" href="InnovaWeeKend.html">INNOVAWEEKEND <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="InnovaHack.html">INNOVAHACK</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="Lideres.html">LÍDERES</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="Aliados.php">ALIADOS</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">REGISTRO</a>
+                  </li>
+       
+               </ul>
+          </div>
+       </nav>
+	
+	
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
