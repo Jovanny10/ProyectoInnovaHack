24c6b296b126ad31874b12f4657ad38cad989747
Jovanny Leobardo Salas <jovannyleobardo10@gmail.com>
2018-08-16 01:12:17 -0500

    Agregandoo imagenes para Aliados y Lideres

--- registro.php
+++ registro.php
@@ -34,26 +34,16 @@
                      <a class="nav-link" href="Lideres.html"><i class="fas fa-user-lock"></i> LÍDERES</a>
                   </li>
                   <li class="nav-item">
-                     <a class="nav-link" href="Aliados.php"><i class="fas fa-user-friends"></i> ALIADOS</a>
-                  </li>
-                  <li class="nav-item">
                      <a class="nav-link" href="registro.php"><i class="fas fa-user-plus"></i> REGISTRO</a>
                   </li>
        
                </ul>
           </div>
        </nav>
-	<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
+ 
 	<script src="js/jquery-3.3.1.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
+
+
