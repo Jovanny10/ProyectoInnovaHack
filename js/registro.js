a48765b5b12deb7685881b776a08a6cc4e118dcc
Jovanny Leobardo Salas <jovannyleobardo10@gmail.com>
2018-08-20 01:43:54 -0500

    Validando datos del formulario

--- js/registro.js
+++ js/registro.js
@@ -1,3 +1,37 @@
+
+$(document).ready(function(){
+  var formulario  = document.getElementsByName('formulario')[0],
+  elementos = formulario.elements,
+  boton = document.getElementById('btn');
+
+  var validarNombre = function(e){
+    if(formulario.nombre.value == 0 || formulario.nombre.value == null || /^\s+$/.test(nombre)){
+      $("#error").html
+        ("<div class='alert alert-danger'><i class='fa fa-close'></i>Favor de verificar el campo nombre *!<button type = 'button' class = 'close' data-dismiss='alert' arial-label='Close'><span aria-hidden='true'>&times;</span></div>");
+      e.preventDefault();
+    }
+  }
+
+  var apellidos = function(e){
+    if(formulario.apellidos.value == 0 || formulario.apellidos.value == null || /^\s+$/.test(apellidos)){
+      $("#error").html
+        ("<div class='alert alert-danger'><i class='fa fa-close'></i>Favor de verificar el campo apellidos *!<button type = 'button' class = 'close' data-dismiss='alert' arial-label='Close'><span aria-hidden='true'>&times;</span></div>");
+      e.preventDefault();
+    }
+  }
+
+  var validar = function(e){
+       validarNombre(e);
+       apellidos(e);
+
+  };
+  formulario.addEventListener("submit",validar);
+
+});
+
+
+
+
 $(document).ready(function(){
     $('#pai').hide();
 	$('#select').on('change',function(){
@@ -11,3 +45,57 @@ $(document).ready(function(){
 
 	});	
 });
+
+
+$(document).ready(function () {
+         $("#rpassword").keyup(checkPasswordMatch);
+ 
+    });
+
+   $(document).ready(function () {
+         $("#password").keyup(checkPasswordMatch2);
+    });
+
+ 
+ 
+
+ function checkPasswordMatch2() {
+     var repeatPass= document.getElementById('rpassword').value;
+var repeatclave = repeatPass.length;
+ if (repeatclave>0)
+ {
+    var password = $("#password").val();
+    var confirmarPassword = $("#rpassword").val();
+
+    if (password != confirmarPassword){
+        $("#divchearsisoniguales").html
+        ("<div class='alert alert-danger'><i class='fa fa-close'></i>  Las contraseñas NO coinciden!<input value='error' type='hidden' name='passwordchecker'></div>"); 
+} else{
+    
+        $("#divchearsisoniguales").html("<div class='alert alert-success'><i class='fa fa-check'></i> Las contraseñas coinciden.<input type='hidden'  value='1' name='passwordchecker'></div>");
+    }
+    }
+}
+
+
+
+function checkPasswordMatch() {
+      var repeatPass= document.getElementById('password').value;
+var repeatclave = repeatPass.length;
+ if (repeatclave>0)
+ {
+    var password = $("#password").val();
+    var confirmarPassword = $("#rpassword").val();
+
+    if (password != confirmarPassword){
+        $("#divchearsisoniguales").html
+        ("<div class='alert alert-danger'><i class='fa fa-close'></i>  Las contraseñas NO coinciden!<input value='error' type='hidden' name='passwordchecker'></div>"); 
+} else{
+    
+        $("#divchearsisoniguales").html("<div class='alert alert-success'><i class='fa fa-check'></i> Las contraseñas coinciden.<input type='hidden'  value='1' name='passwordchecker'></div>");
+    }
+    }
+ 
+}
+
+
--- registro.php
+++ registro.php
@@ -6,37 +6,39 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="css/registro.css">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
   <script src="js/jquery-3.3.1.min.js"></script>
   <script type="text/javascript" src="js/registro.js"></script>
 </head>
-<body>  
+<body>
   <div class="boton text-right">
     <a href="miperfil.php" class="btn btn-danger" role="button" value="registrar">Ir a Mi perfil</a>
   </div>
-	<form class="formulario" name="formulario" method="get">
+	<form class="formulario" name="formulario" id="formulario" method="get" action="prueba.php">
     
 		<div class="container">
       <div class="form-group">
         <h1 class="text-white">Datos personales</h1>
       </div>
-			<div class="form-row">
+			     <div class="form-row">
                 <div class="form-group col-md-6">
-                   <label for="#">Nombre :</label>
+                   <label for="#">Nombre *:</label>
                    <input type="text" class="form-control" id="nombre" placeholder="Ingresar el Nombre">
                 </div>
-                <div class="form-group col-md-6">
-                   <label for="inputPassword4">Apellidos :</label>
-                   <input type="text" class="form-control" id="apellidos" placeholder="Ingresar Apellidos">
+                <div class="form-group col-md-6" id="nombre">
+                   <label for="inputPassword4">Apellidos *:</label>
+                   <input type="text" class="form-control" id="apellidos"  placeholder="Ingresar Apellidos">
                 </div>
            </div>
            <div class="form-row">
                 <div class="form-group col-md-6">
-                   <label for="#">Email :</label>
-                   <input type="email" class="form-control" id="email" placeholder="Labsol@gmail.com /Usuario registro">
+                   <label for="#">Email *:</label>
+                   <input type="email" class="form-control" required="" id="email" placeholder="Labsol@gmail.com /Usuario registro">
                 </div>
+                <div class="" id="checkemailresponse"></div>
                 <div class="form-group col-md-6">
-                   <label for="#">Cel :</label>
-                   <input type="tel" class="form-control" id="cel" placeholder="953-155-01-93">
+                   <label for="#">Cel *:</label>
+                   <input type="tel" class="form-control" id="cel"  placeholder="953-155-01-93">
                 </div>
            </div>
            <div class="form-row">
@@ -63,11 +65,11 @@
            <div class="form-row" >
                 <div class="form-group col-md-6">
                    <label for="#">Facebook :</label>
-                   <input type="text" class="form-control" id="facebook" placeholder="Ingresar cuenta">
+                   <input type="text" class="form-control"  id="facebook" placeholder="Ingresar cuenta">
                 </div>
                 <div class="form-group col-md-6">
                    <label for="#">Carrera :</label>
-                   <input type="text" class="form-control" id="carrera" placeholder="Ingresar Carrera">
+                   <input type="text" class="form-control"  id="carrera" placeholder="Ingresar Carrera">
                 </div>
                 
            </div>
@@ -75,19 +77,19 @@
            <div class="form-row">
                 <div class="form-group col-md-6">
                    <label for="#">Twitter :</label>
-                   <input type="text" class="form-control" id="twitter" placeholder="Ingresar cuenta">
+                   <input type="text" class="form-control"  id="twitter" placeholder="Ingresar cuenta">
                 </div>
                 <div class="form-group col-md-6">
-                   <label for="#">Fecha de Nacimiento :</label>
-                   <input type="date" class="form-control" id="fecha" placeholder="00/ 00 / 00">
+                   <label for="#">Fecha de Nacimiento *:</label>
+                   <input type="date" class="form-control"  id="fecha" placeholder="00/ 00 / 00">
                 </div>
                 
            </div>
 
            <div class="form-row">
                 <div class="form-group col-md-6">
-                   <label for="exampleFormControlSelect1">Sexo :</label>
-                   <select class="form-control" id="exampleFormControlSelect1">
+                   <label for="exampleFormControlSelect1">Sexo *:</label>
+                   <select class="form-control" id="exampleFormControlSelect1" required>
                         <option>Seleccionar</option>
                         <option>Hombre</option>
                         <option>Mujer</option>
@@ -114,14 +116,23 @@
 
            <div class="form-row">
                  <div class="form-group col-md-6">
-                   <label for="#">Contraseña :</label>
-                   <input type="password" class="form-control" id="contraseña" placeholder="********">
+                   <label for="#">Contraseña *:</label>
+                   <input type="password" class="form-control"  id="password" placeholder="********" >
                 </div>
                 <div class="form-group col-md-6">
-                   <label for="#">Confirmar contraseña :</label>
-                   <input type="password" class="form-control" id="contraseña" placeholder="********">
+                   <label for="#">Confirmar contraseña *:</label>
+                   <input type="password" class="form-control" id="rpassword" id="contraseña" placeholder="********">
+
+                </div> 
+           </div>
+           <div class="form-row">
+                 <div class="form-group col-md-6" id="error">
+                   
                 </div>
-                
+                <div class="form-group col-md-6" id="divchearsisoniguales">
+                   
+                </div>
+                 
            </div>
 
   <button type="submit" id="btn" class="btn btn-danger">Registrar</button>
